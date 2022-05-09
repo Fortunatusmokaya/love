121e2e85129ea4426ab7b2cf9b01c43288105723
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-09 09:42:31 +0300

    Update Admin.js

--- plugins/Admin.js
+++ plugins/Admin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
   if (m.fromMe) throw 'no'
-  if (isAdmin) throw '*Master*, You are no longer_An Admin, 😥 Type *.p-owner* anytime and *I will make you Group Admin again 🌚'
+  if (isAdmin) throw 'You just Demoted Yourself, You are no longer_An Admin, 😥 Type *.p-owner* anytime and I will make you *Group Admin* again 🌚'
   await conn.groupDemoteAdmin(m.chat, [m.sender])
 }
 handler.command = /^d-owner$/i
