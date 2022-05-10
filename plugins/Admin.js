83eff8fdfdf33dff1b29b390e3afedbc82856a2a
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-10 08:13:45 +0300

    Delete Admin.js

--- plugins/Admin.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'no'
-  if (isAdmin) throw 'You just Demoted Yourself, You are no longer_An Admin, 😥 Type *.p-owner* anytime and I will make you *Group Admin* again 🌚'
-  await conn.groupDemoteAdmin(m.chat, [m.sender])
-}
-handler.command = /^d-owner$/i
-handler.owner = true
-handler.botAdmin = true
-module.exports = handler
