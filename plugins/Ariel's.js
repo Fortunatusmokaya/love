e9ffc2408b2fd01bdf4afe642e2699c80b9162b5
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-10 08:18:05 +0300

    Update Ariel's.js

--- plugins/Ariel's.js
+++ plugins/Ariel's.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-I am a *bot* you know and so I can't read your tag. Mokaya 🗣️ come and check the message please 😥.
+I am a *BOT* you know and so I can't read your tag. Mokaya 🗣️ come and check them message please 😥.
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@254731545486/i
+handler.customPrefix = /@919402104403/i
 handler.command = new RegExp
 
 module.exports = handler
