428c35fd4c324a1e0bced1b261980ba83acd5947
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-11 16:07:02 +0300

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '🎗️@' + v.replace(/@.+/, '')).join`\n`, null, {
+  m.reply(text + '\n' + users.map(v => '🦄@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
   })
 }
