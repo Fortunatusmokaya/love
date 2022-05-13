1f17621b4286ec475df76f8715e5b2669b763ecc
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-13 09:43:20 +0300

    Update pickk.js

--- plugins/pickk.js
+++ plugins/pickk.js
@@ -1,7 +1,7 @@
 let handler = async (m, { text, args, participants }) => {
     if (args[0] < 0, args.length < 2) throw 'Example: #pick 15 gay'
     let users = participants.map(u => u.jid)
-    m.reply(`*You are Picked as ${text.replace(args, '').trimStart()}*
+    m.reply(`*You are the ${text.replace(args, '').trimStart()} Of This Group*
     
 ${new Array(Math.min(users.length, args[0])).fill().map(() => {
     let index = Math.floor(Math.random() * users.length)
