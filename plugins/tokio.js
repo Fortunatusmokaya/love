a073609cf3561f98085d8586f981e5e6734f3f9c
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-16 10:58:02 +0300

    Update tokio.js

--- plugins/tokio.js
+++ plugins/tokio.js
@@ -1,5 +1,5 @@
 let handler = async m => m.reply(`
-💎 *Dreaded Bot*\n\n🔗 *Info :* Very easy and fast to deploy termux-heroku WhatsApp bot written in Js, Modified Tokio-wabot. GitHub deploy link: GitHub.com/itsajaygaur/tokio-wabot. Join Bots WhatsApp Group for Music Requests: https://chat.whatsapp.com/Bzzntl7j7x98YBZVpc7wiz 
+💎 *Dreaded Bot*\n\n🔗 *Info :* Very easy and fast to deploy termux-heroku WhatsApp bot written in Js, Modified Tokio-wabot. GitHub deploy link: GitHub.com/itsajaygaur/tokio-wabot. Join Bots WhatsApp Group for Music Requests: https://chat.whatsapp.com/KKuPUQSg2KQ6juZvkYm70t 
 `.trim()) // repository
 handler.help = ['dreaded']
 handler.tags = ['info']
