e704108c9e6abc9aff0905f5260d0a3e74f29c6f
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-16 23:13:08 +0300

    Update botstat.js

--- plugins/botstat.js
+++ plugins/botstat.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn }) => {
 ├ *${conn.blocklist.length}* Blocked
 ├ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Banned Chat
 ├ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Banned User
-${conn.battery ? `${conn.battery.value}% Battery 🔋
+${conn.battery ? ${conn.battery.value}% Battery 🔋
 └────
     `.trim())
 }
