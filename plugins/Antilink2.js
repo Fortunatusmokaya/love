6286f9d07f0ca2a8e24155bdf161d1fee59ad375
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-17 00:00:11 +0300

    Create Antilink2.js

--- plugins/Antilink2.js
+++ plugins/Antilink2.js
@@ -1,20 +1,21 @@
 let handler = m => m
 
-let linkRegex = /youtu.be\/([0-9A-Za-z]{20,24})/i
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
   let chat = global.DATABASE.data.chats[m.chat]
-  let isytLink = linkRegex.exec(m.text)
+  let isGroupLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isytLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await this.getName(m.sender)}* you have sent the YouTube  link!\n\nByee Look for subscribers elsewhere!`)
-    if (isrowner) return m.reply('*Hey sorry, I  won't kick my owner,*Mokaya* you won\'t be kicked. hehe..*')
-    if (!isBotAdmin) return m.reply('*Bot is not admin, Otherwise am leaving too -*')
-    let linkyt = ('https://youtu.be/' + await this.youtubeInviteCode(m.chat)))
-    let isLinkThisyt = new RegExp(linkyt, 'i')
-    let isytlink = isLinkThisyt.test(m.text)
-    if (isytlink) return m.reply('*Lol send your own Youtube link :v*')
-    await this.groupRemove(m.chat, [m.sender])
+await this.groupRemove(m.chat, [m.sender])
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await this.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked out from this group byee!`)
+    if (isAdmin) return m.reply('*Hey sorry you\'re admin, you won\'t be kicked. haha..*')
+    if (!isBotAdmin) return m.reply('*Bot is not admin, how can it kick people -_-*')
+    let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
+    let isLinkThisGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkThisGc.test(m.text)
+    if (isgclink) return m.reply('*Lol send your own group link :v*'))
+   
   }
   return true
 }
