8e307722a87c0d142390df4103b2273a77c05111
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-17 17:38:39 +0300

    Update Antilink2.js

--- plugins/Antilink2.js
+++ plugins/Antilink2.js
@@ -7,8 +7,7 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*Detected, *${await this.getName(m.sender)}*, this is a group link! 👀. Leave before I kick 🥱`)
-   if (isrowner) return m.reply('Oops, Sender of Link is *owner* 🧎. I CANNOT remove! 🤝')
+    await m.reply(`*Group Link* Sent!. Leave before I kick 🥱`)
    await this.groupRemove(m.chat, [m.sender])
 
   }
