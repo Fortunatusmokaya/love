199c938c794bf7dd9687545f475be7470fb25c12
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-19 10:32:47 +0300

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -1,19 +1,18 @@
-const { toAudio } = require('../lib/converter')
-const { MessageType } = require('@adiwajshing/baileys')
+import { toAudio } from '../lib/converter.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Balas video atau voice note yang ingin diubah ke mp3 dengan caption *${usedPrefix + command}*`
-  let media = await q.download()
-  let audio = await toAudio(media, 'mp4')
-  conn.sendMessage(m.chat, audio, MessageType.audio, {
-    quoted: m, mimetype: 'audio/mp4'
-  })
+    let q = m.quoted ? m.quoted : m
+    let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+    if (!/video|audio/.test(mime)) throw `reply video/voice note you want to convert to audio/mp3 with caption *${usedPrefix + command}*`
+    let media = await q.download?.()
+    if (!media) throw 'Can\'t download media'
+    let audio = await toAudio(media, 'mp4')
+    if (!audio.data) throw 'Can\'t convert media to audio'
+    conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
-handler.help = ['tomp3 (reply)']
+handler.help = ['tomp3']
 handler.tags = ['audio']
 
 handler.command = /^to(mp3|a(udio)?)$/i
 
-module.exports = handler
+export default handler
