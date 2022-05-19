bc040d0a12277ad8a17c0a260e8f269799cd65ce
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-19 12:26:02 +0300

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -8,7 +8,7 @@ handler.all = async function (m) {
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
             if (this.spam[m.sender].count > 10) {
                 global.db.data.users[m.sender].banned = true
-                m.reply('*Do not Spam!!*')
+                m.reply('Do Not Spam Here. I will kick!!')
 await this.groupRemove(m.chat, [m.sender])
 
             }
