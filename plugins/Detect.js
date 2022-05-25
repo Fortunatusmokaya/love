1598df04de64568b20e3e316c98ce8412714629b
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-25 23:05:53 +0300

    Update Detect.js

--- plugins/Detect.js
+++ plugins/Detect.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-You tagged my *OWNER* Love 🌚 He is busy though ❤️, Hehe. Leave Him Alone 🤝.
+
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
